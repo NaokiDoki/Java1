9193b98ef9b717b046e5f6f1300e850b1387e2f3
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-15 07:13:35 +0545

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,12 @@
+//Printing Bio data
+
+class bio{
+	public static void main(String[] args){
+		System.out.println("Name: Prasiddha Gurung");
+		System.out.println("Address: Naikap, Kathmandu");
+		System.out.println("DOB: 2060/1/22");
+		System.out.println("Gender: Male");
+		System.out.println("Email: grgprasiddha@gmail.com");
+		System.out.println("Phone: 100");
+		}
+	}
