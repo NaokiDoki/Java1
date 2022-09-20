12ecf7456af09c5f299437a9393847f73e416f68
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-20 08:44:11 +0545

    Update 4.java

--- 4.java
+++ 4.java
@@ -3,13 +3,13 @@
 import java.util.*;
 class SI{
 	public static void main(String[] args){
-		Scanner aditi = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter principle: ");
-		double p = aditi.nextInt();
+		double p = scan.nextDouble();
 		System.out.println("Enter Time: ");
-		double t = aditi.nextInt();
+		double t = scan.nextDouble();
 		System.out.println("Enter Rate: ");
-		double r = aditi.nextInt();
+		double r = scan.nextDouble();
 		
 		double SI = (p*t*r)/100;
 		System.out.println("The simple interest is "+SI);
