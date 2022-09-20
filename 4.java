0f62b8e4e5b395181b6011e2670edcc05a81d099
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-20 08:18:14 +0545

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,17 +1,17 @@
 //calculation simple interest 
 
-import java.util.Scanner;
+import java.util.*;
 class SI{
 	public static void main(String[] args){
 		Scanner aditi = new Scanner(System.in);
 		System.out.println("Enter principle: ");
-		float p = aditi.nextInt();
+		double p = aditi.nextInt();
 		System.out.println("Enter Time: ");
-		float t = aditi.nextInt();
+		double t = aditi.nextInt();
 		System.out.println("Enter Rate: ");
-		float r = aditi.nextInt();
+		double r = aditi.nextInt();
 		
-		float SI = (p*t*r)/100;
+		double SI = (p*t*r)/100;
 		System.out.println("The simple interest is "+SI);
 		}
 	}
