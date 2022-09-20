62d3d7af0632f8596c204547ee37bf2d70827ff9
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-20 10:00:49 +0545

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,17 @@
+//calculation Compound interest 
+
+import java.util.*;
+class CI{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter principle: ");
+		double p = scan.nextDouble();
+		System.out.println("Enter Time: ");
+		double t = scan.nextDouble();
+		System.out.println("Enter Rate: ");
+		double r = scan.nextDouble();
+		
+		double CI = p*Math.pow(1+r/100,t);
+		System.out.println("The Compound interest is "+CI);
+		}
+	}
