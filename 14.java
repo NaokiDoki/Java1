ae9e5d0064197645b2be6b6694ffcbf8355b8306
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-27 10:14:27 +0545

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,14 @@
+// multiplication table up to 10 using for loop
+
+class table{
+	public static void main(String[] args){
+		for(int i=1;i<=10;i++){
+			System.out.print("\n");
+			for(int j=1;j<=10;j++){
+				System.out.println(i+" x "+j+" = "+i*j);
+
+			}
+
+		}
+	}
+}
