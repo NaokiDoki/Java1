7719a7940171e2a58e6c74c99a0e2639246f6bf8
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2022-09-29 06:32:31 +0545

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,12 @@
+// printing numbers from 1-10 with their squares
+
+class squares{
+	public static void main(String[] args){
+		System.out.println("number\tSquares");
+		for(int i=1; i<=10; i++){
+			System.out.print(i);
+			System.out.println("\t" + i*i);
+		}
+	}
+}
+		
